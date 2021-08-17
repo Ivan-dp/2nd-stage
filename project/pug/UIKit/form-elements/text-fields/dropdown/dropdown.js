293c3e39332f250b184bc154acaddee3f55262b2
Ivan <ivan-dp@bk.ru>
2021-08-17 21:15:46 +0300

    air-datepicker is done on the UIKit page for own input. Start workong with item-quantity-dropdown

---
+++ project/pug/UIKit/form-elements/text-fields/dropdown/dropdown.js
@@ -0,0 +1,8 @@
+import 'item-quantity-dropdown';
+import 'item-quantity-dropdown/lib/item-quantity-dropdown.css';
+require('item-quantity-dropdown');
+require('item-quantity-dropdown/lib/item-quantity-dropdown.css');
+
+$(document).ready(() => {
+  $('.iqdropdown').iqDropdown({ [options] });
+});
