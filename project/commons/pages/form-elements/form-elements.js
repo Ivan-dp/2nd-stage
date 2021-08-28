7a850e849e41c55b6c2e05d346c2ca6d0b3d44ab
Ivan <ivan-dp@bk.ru>
2021-08-28 22:33:41 +0300

    I configure webpack.config.js

---
+++ project/commons/pages/form-elements/form-elements.js
@@ -0,0 +1,4 @@
+import './form-elements.css';
+import '../../blocks/text-fields/text-fields.css';
+import '../../blocks/filter-date-dropdown/filter-date-dropdown.css';
+import '../../blocks/filter-date-dropdown/dist/css/datepicker.min.css';
--- project/webpack.config.js
+++ project/webpack.config.js
@@ -1,21 +1,61 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
+  context: path.resolve(__dirname, 'commons'),
+  resolve: {
+    alias: {
+      commons: path.resolve(__dirname, './commons/')
+    }
+  }
   mode: 'development',
   entry: {
-    filterDateDropdown: './pug/UIKit/form-elements/text-fields/filter-date-dropdown/filter-date-dropdown.js',
-    dropdown:'./pug/UIKit/form-elements/text-fields/dropdown/dropdown.js'
+    filter_date_dropdown: '@commons/pages/form-elements/form-elements.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
+    filename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    // new HtmlWebpackPlugin({
+    //   filename: 'html/cards.html',
+    //   template: '/pages/cards/cards.pug',
+    //   chunks: ['cards']
+    // }),
+    // new HtmlWebpackPlugin({
+    //   filename: 'html/colors-and-types.html',
+    //   template: '/pages/colors-and-types/colors-and-types.pug',
+    //   chunks: ['cards']
+    // }),
     new HtmlWebpackPlugin({
-      filename: 'html/UIKit.html',
-      template: './pug/UIKit/UIKit.pug', // шаблон
-      chunks: ['UIKit'],
+      filename: 'html/form-elements.html',
+      template: '/pages/form-elements/form-elements.pug',
+      chunks: ['cards']
     }),
-  ]
-};
+    // new HtmlWebpackPlugin({
+    //   filename: 'html/headers-and-footers.html',
+    //   template: '/pages/headers-and-footers/headers-and-footers.pug',
+    //   chunks: ['cards']
+    // }),
+    new CleanWebpackPlugin()    
+  ],
+  stats: {
+    children: true,
+  },
+  module: {
+    rules: [
+      // scss
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
+      },
+      // pug
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      }
+    ]
+  }
+}
