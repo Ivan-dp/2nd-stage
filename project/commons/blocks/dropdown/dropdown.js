67daefdacfd36fec5d4220026ba3d05a073b9c71
Ivan <ivan-dp@bk.ru>
2021-09-01 18:09:06 +0300

    text-fields is done. Start to make checkboxes

--- project/commons/blocks/dropdown/dropdown.js
+++ project/commons/blocks/dropdown/dropdown.js
@@ -4,5 +4,46 @@ require('./item-quantity-dropdown.min');
 require('../../../node_modules/item-quantity-dropdown/lib/item-quantity-dropdown.min.css');
 
 $(document).ready(() => {
-  $('.iqdropdown').iqDropdown({ });
+  $('.iqdropdown').iqDropdown({ 
+    // selectionText: 'Сколько гостей',
+    // textPlural: 'гость',
+    textPluralDeclination:['гость','гостя','гостей'],
+    items: {},
+   });
 });
+
+
+var parent = document.querySelector('.iqdropdown-menu');
+
+var btnBlock = document.createElement('div');
+parent.appendChild(btnBlock);
+btnBlock.className = 'button-block';
+
+
+var clearBtn = document.createElement('button');
+clearBtn.innerHTML = '<p>Очистить</p>';
+btnBlock.appendChild(clearBtn);
+clearBtn.className = 'clear-button';
+
+
+var applyBtn = document.createElement('button');
+applyBtn.innerHTML = '<p>Применить</p>';
+btnBlock.appendChild(applyBtn);
+applyBtn.className = 'apply-button';
+
+
+
+// var parent = document.querySelector('.iqdropdown-item-controls');
+// // var closestParent = parent.closest("div");
+// var iDecr = document.querySelector('.icon-decrement');
+
+// // btnDecr.removeChild(iDecr);
+// // iqdropdown-item-controls
+// parent.removeChild(iDecr);
+
+// var btnDecr = document.querySelector('button.button-decrement')
+
+// var pDecr = document.createElement('p');
+// pDecr.innerHTML = '-';
+
+// btnDecr.appendChild(pDecr);
--- project/commons/pages/form-elements/form-elements.js
+++ project/commons/pages/form-elements/form-elements.js
@@ -1,6 +1,6 @@
-import './form-elements.scss';
 import '../../blocks/dropdown/dropdown';
 import '../../blocks/dropdown/item-quantity-dropdown.min';
 import '../../blocks/date-dropdown/date-dropdown';
 import '../../blocks/filter-date-dropdown/filter-date-dropdown';
 import '../../blocks/filter-date-dropdown/dist/js/datepicker.min';
+import './form-elements.scss';
