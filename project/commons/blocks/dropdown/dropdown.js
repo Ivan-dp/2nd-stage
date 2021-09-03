d9296773d0687dedc63d1a3658d2c22bc1403d20
Ivan <ivan-dp@bk.ru>
2021-09-04 00:28:26 +0300

    3-4.09.21 add 3 dropdowns to ui-kit

--- project/commons/blocks/dropdown/dropdown.js
+++ project/commons/blocks/dropdown/dropdown.js
@@ -13,37 +13,20 @@ $(document).ready(() => {
 });
 
 
-var parent = document.querySelector('.iqdropdown-menu');
+// var parent = document.querySelector('.iqdropdown-menu');
 
-var btnBlock = document.createElement('div');
-parent.appendChild(btnBlock);
-btnBlock.className = 'button-block';
-
-
-var clearBtn = document.createElement('button');
-clearBtn.innerHTML = '<p>Очистить</p>';
-btnBlock.appendChild(clearBtn);
-clearBtn.className = 'clear-button';
+// var btnBlock = document.createElement('div');
+// parent.appendChild(btnBlock);
+// btnBlock.className = 'button-block';
 
 
-var applyBtn = document.createElement('button');
-applyBtn.innerHTML = '<p>Применить</p>';
-btnBlock.appendChild(applyBtn);
-applyBtn.className = 'apply-button';
+// var clearBtn = document.createElement('button');
+// clearBtn.innerHTML = '<p>Очистить</p>';
+// btnBlock.appendChild(clearBtn);
+// clearBtn.className = 'clear-button';
 
 
-
-// var parent = document.querySelector('.iqdropdown-item-controls');
-// // var closestParent = parent.closest("div");
-// var iDecr = document.querySelector('.icon-decrement');
-
-// // btnDecr.removeChild(iDecr);
-// // iqdropdown-item-controls
-// parent.removeChild(iDecr);
-
-// var btnDecr = document.querySelector('button.button-decrement')
-
-// var pDecr = document.createElement('p');
-// pDecr.innerHTML = '-';
-
-// btnDecr.appendChild(pDecr);
+// var applyBtn = document.createElement('button');
+// applyBtn.innerHTML = '<p>Применить</p>';
+// btnBlock.appendChild(applyBtn);
+// applyBtn.className = 'apply-button';
