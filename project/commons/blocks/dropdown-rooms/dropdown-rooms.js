5a4da35a45b7f6125f6dd243a7db6fd215608511
Ivan <ivan-dp@bk.ru>
2021-09-13 02:08:37 +0300

    13.09.21 text plural declination for dropdown is done

--- project/commons/blocks/dropdown-rooms/dropdown-rooms.js
+++ project/commons/blocks/dropdown-rooms/dropdown-rooms.js
@@ -1,25 +1,89 @@
-// var dr = document.querySelector('.dropdown-rooms');
-// var d1 = dr.querySelectorAll('.dropdown-1');
-// var iqd1 = d1[0].querySelector('.iqdropdown');
-// var iqd2 = d1[1].querySelector('.iqdropdown .menu-open');
-
 var rooms = document.querySelectorAll('#rooms');  
 
-  function getNameEachId2(array) {
-    for (var i = 0; i < array.length; i++) {
-      var myVar = array[i];
-      myVar.setAttribute('id', 'rooms_'+[i+1]);
-      console.log(myVar);
-    }
+// Функция для задания ID дропдаунам 
+
+function getNameEachId2(array) {
+  for (var i = 0; i < array.length; i++) {
+    var myVar = array[i];
+    myVar.setAttribute('id', 'rooms_'+[i+1]);
+    console.log(myVar);
   }
-  getNameEachId2(rooms);
+}
+getNameEachId2(rooms);
 
-  var r1 = document.querySelector('#rooms_1');
-  var r2 = document.querySelector('#rooms_2');
 
-  console.log(r1, r2);
-  console.log(r1.id, r2.id);
+// Присваиваем селекторы значениям переменных
 
+var r1 = document.querySelector('#rooms_1');
+var r2 = document.querySelector('#rooms_2');
+
+var n = r1.querySelectorAll('.iqdropdown-menu .iqdropdown-menu-option');
+
+var n0 = n[0];
+var n1 = n[1];
+var n2 = n[2];
+
+
+var m = r2.querySelectorAll('.iqdropdown-menu .iqdropdown-menu-option');
+
+var m0 = m[0];
+var m1 = m[1];
+var m2 = m[2];
+
+// Задаём массивы слов со склонениями для функции выбора слов с правильным склонением
+
+var arr1 = ['Спальня', 'Спальни', 'Спален'];
+var arr2 = ['Кровать', 'Кровати', 'Кроватей'];
+var arr3 = ['Ванная комната', 'Ванные комнаты', 'Ванных комнат'];
+
+
+//Функция для выбора слова с правильным склонением из заготовленного массива слов
+
+function loop(someCount, textArr) {
+  var text;
+  if (someCount == 1) {
+    text = someCount + ' ' + textArr[0];
+  } else if (someCount > 1 && someCount < 5) {
+    text = someCount + ' ' + textArr[1];
+  } else if (someCount >= 5) {
+    text = someCount + ' ' + textArr[2];
+  } else {
+    text = '';
+  }
+  return text;
+}
+
+
+// Функция сложения строк счётчиков в одну
+
+function varLength(var1, var2, var3) {
+  var newText = 'Выберите удобства';
+  if (var1.length > 0) {
+    newText = '';
+    newText += var1;
+    if (var2.length > 0) {
+      newText = newText + ', ' + var2;
+      if (var3.length > 0) {
+        newText = newText + ', ' + var3;
+      }
+    } else if (var3.length > 0) {
+      newText = newText + ', ' + var3;
+    }
+  } else if (var2.length > 0) {
+    newText = '';
+    newText += var2;
+    if (var3.length > 0) {
+      newText = newText + ', ' + var3;
+    }
+  } else if (var3.length > 0) { 
+    if (var1.length > 0) {
+      newText = var1 + ', ' + var3;
+    }
+    newText = '';
+    newText += var3;
+  }
+  return newText;
+}
 
 $(document).ready(() => {
   $(r1).iqDropdown({ 
@@ -28,36 +92,24 @@ $(document).ready(() => {
       items: {},
       onChange: (id, count, totalItems) => {
         console.log(totalItems, count, id);
-        if (id === 'rooms1') {
-          var st;
-          if (count == 1) {
-            st = 'Спальня';
-          } else if (count > 1 && count < 5) {
-            st = 'Спальни';
-          } else if (count >= 5) {
-            st = 'Спален';
-          }
-        } else if (id === 'rooms2') {
-          var sm;
-          if (count == 1) {
-            sm = 'Кровать';
-          } else if (count > 1 && count < 5) {
-            sm = 'Кровати';
-          } else if (count >= 5) {
-            sm = 'Коватей';
-          }
-        } else if (id === 'rooms3') {
-          var sp;
-          if (count == 1) {
-            sp = 'Ванная комната';
-          } else if (count > 1 && count < 5) {
-            sp = 'Ванные комнаты';
-          } else if (count >= 5) {
-            sp = 'Ванных комнат';
-          }
-        }
-        r1.querySelector('.iqdropdown-selection').innerHTML = count + ' ' + st + ', ' + count + ' ' + sm + ', ' + count + ' ' + sp;
-        console.log(count.id);
+        var nn0 = n0.querySelector('span.counter');
+        var nn1 = n1.querySelector('span.counter');
+        var nn2 = n2.querySelector('span.counter');
+
+        var counter1 = nn0.outerText;
+        var counter2 = nn1.outerText;
+        var counter3 = nn2.outerText;
+
+        console.log(counter1, counter2, counter3);
+
+        var text1 = loop(counter1, arr1);
+        var text2 = loop(counter2, arr2);
+        var text3 = loop(counter3, arr3);
+        
+        newText = varLength(text1, text2, text3);
+        console.log(newText);
+        console.log(text1.length, text2.length, text3.length);
+        r1.querySelector('.iqdropdown-selection').innerHTML = newText;
       },
       setSelectionText: (itemCount, totalItems) => {
       }
@@ -68,19 +120,27 @@ $(document).ready(() => {
       items: {},
       onChange: (id, count, totalItems) => {
         console.log(totalItems, count, id);
-        var tp;
-        if (totalItems === 0) {
-          return;
-        } else if (totalItems >= 5) {
-          tp = 'гостей';
-        } else if ((totalItems > 1) && (totalItems < 5)) {
-          tp = 'гостя';
-        } else if (totalItems == 1) {
-          tp = 'гость';
-        }
-        r2.querySelector('.iqdropdown-selection').innerHTML = totalItems + ' ' + tp;
+        var mm0 = m0.querySelector('span.counter');
+        var mm1 = m1.querySelector('span.counter');
+        var mm2 = m2.querySelector('span.counter');
+
+        var counter1 = mm0.outerText;
+        var counter2 = mm1.outerText;
+        var counter3 = mm2.outerText;
+
+        console.log(counter1, counter2, counter3);
+
+        var text1 = loop(counter1, arr1);
+        var text2 = loop(counter2, arr2);
+        var text3 = loop(counter3, arr3);
+        
+        newText = varLength(text1, text2, text3);
+        console.log(newText);
+        console.log(text1.length, text2.length, text3.length);
+        r2.querySelector('.iqdropdown-selection').innerHTML = newText;
       },
       setSelectionText: (itemCount, totalItems) => {
       }
     });
+  
 });
