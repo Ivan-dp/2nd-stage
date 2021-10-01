dca72e301e7d0ddd09a75b484dd105ca4a8065f0
Ivan <ivan-dp@bk.ru>
2021-10-01 15:39:12 +0300

    1.10.21 redid the folders and pug-files according to the BEM methodology. Advantage, auth, bullet-list, buttons, checkbox-list, colors.

--- project/src/blocks/checkbox-list/checkbox-list.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('open').checked=true;
--- project/src/pages/form-elements/form-elements.js
+++ project/src/pages/form-elements/form-elements.js
@@ -1,4 +1,4 @@
 import "../../blocks/text-fields/text-fields";
 import "../../blocks/range-slider/range-slider";
-import "../../blocks/checkbox-list/checkbox-list";
+import "../../blocks/checkbox-list/checkbox-list_checked";
 import './form-elements.scss';
